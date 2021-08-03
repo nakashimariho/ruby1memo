3f892ea1d66a2628093a5e3214fcdb0491b88dff
EC2 Default User <ec2-user@ip-172-31-34-154.us-east-2.compute.internal>
2021-08-03 12:59:43 +0000

    chomp

--- memo.rb
+++ memo.rb
@@ -5,7 +5,7 @@ memo_type = gets.chomp
 
 if memo_type == "1"
     puts "拡張子を除いたファイルを入力してください"
-    file_name = gets.to_s
+    file_name = gets.chomp
     puts "メモしたい内容を記入してください"
     puts "完了したらCtrl + Dを押します"
     
@@ -18,7 +18,7 @@ if memo_type == "1"
     
 elsif memo_type == "2"
     puts "編集したいファイル名を入力してください"
-    file_name = gets.to_s
+    file_name = gets.chomp
     
     puts "編集内容を記入してください"
     puts "完了したらCtrl + Dを押します"
